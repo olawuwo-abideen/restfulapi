6d98e2b2581318773a800f4f46fa9164a581d12e
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-12 02:09:50 +0100

    database setup

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-//TODO
+
+mongoose.connect("mongodb://localhost:27017/newsDB");
+
+const articleSchema = {
+  headline: String,
+  content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
