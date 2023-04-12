83d11a89aa9b4bd82dce3a99b69df4a83b38b7af
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-04-12 03:31:38 +0100

    get

--- app.js
+++ app.js
@@ -63,7 +63,22 @@ app.route("/articles")
       }
     });
   });
+
+  // for a specific articles
   
+  app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if (foundArticle) {
+      res.send(foundArticle);
+    } else {
+      res.send("No articles matching that title was found.");
+    }
+  });
+})
+
  
 
 app.listen(3000, function() {
